a79330d9e0f89fbc582eb5dd8156a6dc4b30ea32
ClementAver <clement.aver@yahoo.fr>
2023-10-28 10:45:59 +0200

    Custom Select component -> translated HTML into React and added styles.

--- src/components/inputs/Select/Select.tsx
+++ src/components/inputs/Select/Select.tsx
@@ -3,8 +3,14 @@ import { SelectProps, Option } from "../inputs";
 export default function Select({ inputsState, inputState, setInputsState, showValidation, id, label, placeholder, options, validationMsg }: SelectProps) {
   return (
     <>
-      <label htmlFor={id}>{label}</label>
+      {/* <label
+        className="native-select__label"
+        htmlFor={id}
+      >
+        {label}
+      </label>
       <select
+        className="native-select native-select__select"
         name={id}
         id={id}
         value={inputsState[inputState as keyof typeof inputsState].toString()}
@@ -20,7 +26,55 @@ export default function Select({ inputsState, inputState, setInputsState, showVa
           </option>
         ))}
       </select>
-      {showValidation && <p className="invalid">{validationMsg}</p>}
+      {showValidation && <p className="native-select__invalid-msg invalid">{validationMsg}</p>} */}
+
+      <div className="select">
+        <span
+          className="selectLabel"
+          id={id}
+        >
+          {label}
+        </span>
+        <div className="selectWrapper">
+          <select
+            className="selectNative js-selectNative"
+            aria-labelledby={id}
+            name={id}
+            value={inputsState[inputState as keyof typeof inputsState].toString()}
+            onChange={(e) => setInputsState({ ...inputsState, [inputState]: e.target.value })}
+          >
+            <option value="">{placeholder}</option>
+            {options.map((option: Option, index: number) => (
+              <option
+                key={index}
+                value={option.value}
+              >
+                {option.labor}
+              </option>
+            ))}
+          </select>
+
+          {/* Hide the custom select from AT (e.g. SR) using aria-hidden */}
+          <div
+            className="selectCustom js-selectCustom"
+            aria-hidden="true"
+          >
+            <div className="selectCustom-trigger">{placeholder}</div>
+            <div className="selectCustom-options">
+              {options.map((option: Option, index: number) => (
+                <div
+                  className="selectCustom-option"
+                  key={index}
+                  data-value={option.value}
+                >
+                  {option.labor}
+                </div>
+              ))}
+            </div>
+          </div>
+          {showValidation && <p className="native-select__invalid-msg invalid">{validationMsg}</p>}
+        </div>
+      </div>
     </>
   );
 }
